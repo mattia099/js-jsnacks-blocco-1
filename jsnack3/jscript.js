bb4aa0f9235751867c46a4a2a4babe2821539539
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-02-11 17:45:12 +0100

    snack3

---
+++ jsnack3/jscript.js
@@ -0,0 +1,18 @@
+let somma = 0;
+//for(let i=0; i<5; i++){
+//    const num = parseFloat(prompt('Inserisci numero da sommare'));
+//    somma= somma+num;
+//}
+
+let i=0;
+while(i<5){
+    const num = parseFloat(prompt('Inserisci numero da sommare'));
+    somma = somma+num;
+    i++
+   
+}
+
+console.log(somma);
+
+
+
